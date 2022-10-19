b81ce38bdaedfb270fd5cf6bf6dcd4344d5e21ac
Tarsis Costa Cruz <costacruzt@gmail.com>
2022-10-19 13:27:59 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className='text-2xl'>Hello Word</h1>
+      <button className="bg0">Enviar</button>
+    </>
+  );
+}
